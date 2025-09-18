bb506a636696f5915bd342d2588332a0ed1be7e9
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-09-18 10:50:02 +0700

    first commit

---
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/CanimEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.canim_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanimEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanimEcommerceApplication.class, args);
+	}
+
+}
