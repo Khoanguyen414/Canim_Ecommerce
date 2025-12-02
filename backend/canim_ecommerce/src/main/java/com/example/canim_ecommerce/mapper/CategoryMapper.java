5a9781bfed970f30368dcc9ba29f752567177f01
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-12-02 23:30:16 +0700

    feat(mapper): add partial update support for Category

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/mapper/CategoryMapper.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/mapper/CategoryMapper.java
@@ -1,9 +1,13 @@
 package com.example.canim_ecommerce.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.example.canim_ecommerce.dto.request.category.CategoryCreationRequest;
+import com.example.canim_ecommerce.dto.request.category.CategoryUpdateRequest;
 import com.example.canim_ecommerce.dto.response.CategoryResponse;
 import com.example.canim_ecommerce.entity.Category;
 
@@ -18,4 +22,11 @@ public interface CategoryMapper {
     @Mapping(target = "parentId", source = "parent.id")
     @Mapping(target = "parentName", source = "parent.name")
     CategoryResponse toCategoryResponse(Category category);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "parent", ignore = true)
+    @Mapping(target = "children", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    void updateCategory(@MappingTarget Category category, CategoryUpdateRequest request);
 }
