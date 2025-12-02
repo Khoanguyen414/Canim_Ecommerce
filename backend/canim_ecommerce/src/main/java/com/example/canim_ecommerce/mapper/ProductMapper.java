d3ebb12be5bce04c66d1cfb03afb8db8b66ad224
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-12-02 23:35:05 +0700

    feat(mapper): add ProductMapper

---
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/mapper/ProductMapper.java
@@ -0,0 +1,41 @@
+package com.example.canim_ecommerce.mapper;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.example.canim_ecommerce.dto.request.product.ProductCreationRequest;
+import com.example.canim_ecommerce.dto.request.product.ProductUpdateRequest;
+import com.example.canim_ecommerce.dto.response.ProductResponse;
+import com.example.canim_ecommerce.entity.Product;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "sku", ignore = true)
+    @Mapping(target = "slug", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    Product toProduct(ProductCreationRequest request);
+
+    @Mapping(target = "categoryId", ignore = true)
+    @Mapping(target = "categoryName", ignore = true)
+    @Mapping(target = "categorySlug", ignore = true)
+    ProductResponse toProductResponse(Product product);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "sku", ignore = true)
+    @Mapping(target = "slug", ignore = true)     
+    @Mapping(target = "category", ignore = true) 
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateProduct(@MappingTarget Product product, ProductUpdateRequest request);
+}
