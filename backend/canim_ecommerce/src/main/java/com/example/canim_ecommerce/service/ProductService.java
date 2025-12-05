d2416ee12505206327ffecd5d1ae49e0521dce8c
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-12-05 23:37:19 +0700

    feat(service): add interface ProductService with CRUD methods and changeProductStatus

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/service/ProductService.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.example.canim_ecommerce.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.example.canim_ecommerce.dto.request.products.ProductCreationRequest;
+import com.example.canim_ecommerce.dto.request.products.ProductUpdateRequest;
+import com.example.canim_ecommerce.dto.response.ProductResponse;
+import com.example.canim_ecommerce.enums.ProductStatus;
+
 public interface ProductService {
+    Page<ProductResponse> getAllProducts(Pageable pageable);
+    ProductResponse getProductById(Long id);   
+    ProductResponse getProductBySku(String sku);   
+    ProductResponse getProductBySlug(String slug);
+    ProductResponse createProduct(ProductCreationRequest request);
+    ProductResponse updateProduct(Long id, ProductUpdateRequest request);
+    void changeProductStatus(Long id, ProductStatus status);
+    void deleteProduct(Long id);    
 }
