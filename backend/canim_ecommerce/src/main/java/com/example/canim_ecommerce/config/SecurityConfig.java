64cfb55d46d88b6b0429455766a05aa1aba115bc
Khoanguyen414 <huykhoanguyen0@gmail.com>
2025-12-26 22:08:18 +0700

    config(security): add /products/** to public endpoints

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/config/SecurityConfig.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
         "/v3/api-docs/**",
         "/swagger-ui/**",
         "/swagger-ui.html",
-        "/categories/**"
+        "/categories/**",
+        "/products/**"
     };
 
     @Bean
