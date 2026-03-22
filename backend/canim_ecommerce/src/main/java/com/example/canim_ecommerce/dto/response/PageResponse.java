555984173f5870b23e4373ac93ed721140224992
Khoanguyen414 <huykhoanguyen0@gmail.com>
2026-03-22 10:35:10 +0700

    feat: Update PageResponse

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/dto/response/PageResponse.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/dto/response/PageResponse.java
@@ -1,39 +1,23 @@
 package com.example.canim_ecommerce.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.*;
 import java.util.List;
-@Getter
-@Setter
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class PageResponse<T> {
-    // Danh sách items trên trang hiện tại
-    private List<T> content;
-    //số trang hiện tại 
-    private Integer currentPage;
-    //số mặt hàng trên mỗi trang
-    private Integer pageSize;
-    //Tổng số mặt hàng
-    private Long totalElements;
-    //Tổng số trang
-    private Integer totalPages;
-    private Boolean hasNext;
-    //Có trang trước hay không
-    private Boolean hasPrevious;
-    
-    // Chuyển đổi từ Page<T> của Spring Data sang PageResponse<T>
-    public static <T> PageResponse<T> of(org.springframework.data.domain.Page<T> page) {
-        return PageResponse.<T>builder()
-            .content(page.getContent())
-            .currentPage(page.getNumber())
-            .pageSize(page.getSize())
-            .totalElements(page.getTotalElements())
-            .totalPages(page.getTotalPages())
-            .hasNext(page.hasNext())
-            .hasPrevious(page.hasPrevious())
-            .build();
-    }
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageResponse<T> { 
+    int page;         
+    int size;      
+    long totalElements;
+    int totalPages;  
+    List<T> data;      
 }
