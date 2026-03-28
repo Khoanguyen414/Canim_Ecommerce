c37a6106c8e8715a44172306f239584e7c44d57d
Khoanguyen414 <huykhoanguyen0@gmail.com>
2026-03-28 15:24:42 +0700

    Update Code Main After merge

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/entity/Product.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/entity/Product.java
@@ -62,18 +62,14 @@ public class Product {
     @Column(nullable = false)
     BigDecimal price;
 
+    @Column(length = 100)
+    String brand;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "brand_id")
-    Brand brand;
+    @Column(length = 50)
+    String color;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "color_id")
-    Color color;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "size_id")
-    Size size;
+    @Column(length = 50)
+    String size;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
