ae7c357a53ef3a110a137ee196da49dc604e636d
Khoanguyen414 <huykhoanguyen0@gmail.com>
2026-04-30 00:10:37 +0700

    feat: add method to find main product image by product ID

--- backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/repository/ProductImageRepository.java
+++ backend/canim_ecommerce/src/main/java/com/example/canim_ecommerce/repository/ProductImageRepository.java
@@ -1,5 +1,7 @@
 package com.example.canim_ecommerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.canim_ecommerce.entity.ProductImage;
 
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long>{
-
+    Optional<ProductImage> findByProductIdAndIsMainTrue(Long productId);
 }
